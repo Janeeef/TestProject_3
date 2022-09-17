a831f3591237514a174fc45e598a81dfbd994b64
Janeeef <janeeef@gmail.com>
2022-09-17 11:27:37 +0300

    js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// Setup
-var a;
-a = 7;
-var b;
-b = a;
 // Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
